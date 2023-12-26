e7a4104195e1f19da102193dd9726bec3a4719ec
rizalpahlevii <mrizalpahlevi372@gmail.com>
2023-12-27 04:54:02 +0700

    Add adminlte

--- database/migrations/2023_12_26_210213_create_dokter_table.php
+++ database/migrations/2023_12_26_210213_create_dokter_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('alamat');
             $table->integer('no_hp');
-            $table->string('id_poli');
+            $table->foreignId('id_poli')->constrained('poli');
         });
     }
 
