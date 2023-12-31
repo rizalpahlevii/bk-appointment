465b80121244f65db849acaa7aeafc2f59c04c1d
rizalpahlevii <mrizalpahlevi372@gmail.com>
2024-01-01 02:54:34 +0700

    Relation

--- app/Models/DetailPeriksa.php
+++ app/Models/DetailPeriksa.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DetailPeriksa extends Model
 {
-    use HasFactory;
+    public $timestamps = false;
 
     protected $table = 'detail_periksa';
 
-    public $timestamps = false;
+    protected $fillable = [
+        'id_periksa',
+        'id_obat',
+    ];
+
+    public function periksa()
+    {
+        return $this->belongsTo(Periksa::class, 'id_periksa');
+    }
+
+    public function obat()
+    {
+        return $this->belongsTo(Obat::class, 'id_obat');
+    }
 }
--- app/Models/Periksa.php
+++ app/Models/Periksa.php
@@ -9,7 +9,24 @@ class Periksa extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_daftar_poli',
+        'tgl_periksa',
+        'catatan',
+        'biaya_periksa',
+    ];
+
     protected $table = 'periksa';
 
-    public $timestamps = false;
+    public function daftarPoli()
+    {
+        return $this->belongsTo(DaftarPoli::class, 'id_daftar_poli');
+    }
+
+    public function detailPeriksa()
+    {
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa');
+    }
 }
