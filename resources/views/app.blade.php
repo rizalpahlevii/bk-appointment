7430db58a954e767e335ec005979b03dbc02f728
rizalpahlevii <mrizalpahlevi372@gmail.com>
2024-01-01 10:26:20 +0700

    Remove loading screen

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,6 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo"
-             height="60" width="60">
-    </div>
 
     <!-- Navbar -->
     @include('partials.header')
